a7fc01d01522967f99e88ee8f34fbdc5dcc0ad5f
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-09-30 20:36:21 -0700

    Refined Logger Calls

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import logging
 import pandas as pd
 
-logger = logging.getLogger(__name__)
 try:
   logging.basicConfig(
     filename='../logs/app.log', 
     level=logging.INFO, 
+    filemode='w',
     format='%(asctime)s | %(levelname)s:\n\t%(message)s'
   )
 except Exception as e:
@@ -22,22 +22,21 @@ def main():
   
   try:
     df = pd.read_csv(file_path)
-    logger.info(f'Successfully read the file at \'{file_path}\'.')
+    logging.info(f'Successfully read the file at \'{file_path}\'.')
 
   except FileNotFoundError:
-    logger.exception(f'Error: The file at \'{file_path}\' was not found.')
+    logging.error(f'The file at \'{file_path}\' was not found.')
     exit(1)
   
   except pd.errors.EmptyDataError:
-    logger.exception(f'Error: The file at \'{file_path}\' is empty.')
-    exit(1)
+    logging.warning(f'The file at \'{file_path}\' is empty.')
   
   except pd.errors.ParserError as e:
-    logger.exception(f'Error: There was a parsing error while reading the file:\n\t{e}')
+    logging.error(f'There was a parsing error while reading the file:\n\t{e}')
     exit(1)
 
   except Exception as e:
-    logger.exception(f'An unexpected error occurred:\n\t{e}')
+    logging.error(f'An unexpected error occurred:\n\t{e}')
     exit(1)
 
 

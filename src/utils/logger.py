9f51ff4e9f2fb7ba75d8da8557197ad185d690aa
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-09-30 21:18:00 -0700

    Added Check for Log File Existence
    Creates log file if it doesn't exist

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,9 +1,16 @@
 import logging
+import os
 
 def setup():
   try:
+    log_file = '../logs/app.log'
+
+    if not os.path.isfile(log_file):
+      with open(log_file, 'w') as f:
+        pass
+
     logging.basicConfig(
-      filename='../logs/app.log', 
+      filename=log_file, 
       level=logging.INFO, 
       filemode='w',
       format='%(asctime)s | %(levelname)s:\n\t%(message)s'

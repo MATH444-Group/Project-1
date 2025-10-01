908f9672d1cefc77baf8e1edc23839fcfd76eb3f
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-09-30 21:43:35 -0700

    Logs Added During Logging Initialization
    Logs are stored in dictionary variable with their associated level
    Logs are recorded to log file after logging initialization

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -5,12 +5,24 @@ def setup():
   try:
     log_dir = '../logs'
     log_file = '../logs/app.log'
+    log_setup_info = {
+      logging.WARNING: [],
+      logging.INFO: []
+    }
+
+
 
     if not os.path.isfile(log_file):
+      log_setup_info[logging.WARNING].append('Log file missing')
       if not os.path.isdir(log_dir):
+        log_setup_info[logging.WARNING].append('Log directory missing')
         os.mkdir(log_dir)
+        log_setup_info[logging.INFO].append(f'Successfully created log directory at \'{log_dir}\'')
+
       with open(log_file, 'w') as f:
-        pass
+        log_setup_info[logging.INFO].append(f'Successfully created log file at \'{log_file}\'')
+
+
 
     logging.basicConfig(
       filename=log_file, 
@@ -18,6 +30,12 @@ def setup():
       filemode='w',
       format='%(asctime)s | %(levelname)s:\n\t%(message)s'
     )
+
+    
+
+    for log_level in log_setup_info:
+      for msg in log_setup_info[log_level]:
+        logging.log(level=log_level, msg=msg)
 
     logging.info('Successfully setup logger.')
 

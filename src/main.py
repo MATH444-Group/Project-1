076bf3bead0c1b37ecb18d6094be80fa71e2b5bb
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-10-01 07:16:05 -0700

    Added Simple Logging to Git Changes in utils

--- src/main.py
+++ src/main.py
@@ -2,12 +2,21 @@ import logging
 import os
 import pandas as pd
 import sys
+import time
 
 if not os.path.isdir('utils'):
   os.system('git clone https://github.com/GevChalikyan/utils.git')
 else:
+  
+  git_log_file_relative_to_utils = '../../logs/git.log'
+  git_log_dir_relative_to_utils = '../../logs'
+
+  if not os.path.isdir(git_log_dir_relative_to_utils):
+    os.mkdir(git_log_dir_relative_to_utils)
+
   os.chdir('utils')
-  os.system('git pull')
+  os.system(f'printf \'{time.asctime()}:\\n\' >> {git_log_file_relative_to_utils} 2>&1')
+  os.system(f'git pull >> {git_log_file_relative_to_utils} 2>&1')
   os.chdir('..')
 
 sys.path.append('utils')

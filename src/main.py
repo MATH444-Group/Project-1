d23364ba1fec040a40e25c75960b051bf4b9ccfb
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-12-04 21:26:54 -0800

    Moved Data Handling Functions to util

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ import time
 
 import __init__
 
-import data_loader as dl
+import dataHandler as dh
 
 
 
@@ -28,68 +28,13 @@ CLEAN_DATA_PATH: os.path = os.path.join(CLEAN_DATA_DIR, CLEAN_DATA_FILE)
 
 
 
-def hasNull(df):
-
-  numNullValues = df.isna().sum().sum()
-  
-  if numNullValues != 0:
-    print(f'{numNullValues} null values found!')
-    return True
-  return False
-
-def cleanData(df):
-
-  if hasNull(df):
-
-    # Drops attributes with more than 1/3 null values
-    THRESHOLD: int = df.shape[0] - (df.shape[0] / 3)
-    print(f'Dropping columns with less than {THRESHOLD} valid values...')
-    INITIAL_COL = df.shape[1]
-
-    df = df.dropna(axis=1, thresh=THRESHOLD)
-
-    COL_DROPPED = INITIAL_COL - df.shape[1]
-    print(f'Successfully dropped {COL_DROPPED} columns...')
-
-
-
-
-
-    # Drops all rows with ANY null values
-    print('Dropping any remaining rows with null values...')
-    INITIAL_ROWS = df.shape[0]
-    print(f'Initial # of Rows: {INITIAL_ROWS}')
-
-    df = df.dropna(axis=0)
-
-    ROWS_DROPPED = INITIAL_ROWS - df.shape[0]
-    print(f'Successfully dropped {ROWS_DROPPED} rows...')
-
-  return df
-
-
-
-
-
-def exportData(df, data_path):
-  df.to_csv(data_path, index=False)
-
-
-
-
-
-
-
 def main():
   
-  df = dl.load_df(RAW_DATA_PATH)
+  df = dh.importData(RAW_DATA_PATH)
 
-  df = cleanData(df)
+  df = dh.cleanData(df)
 
-  print(df.head())
-  print(df.shape[0])
-
-  exportData(df, CLEAN_DATA_PATH)
+  dh.exportData(df, CLEAN_DATA_PATH)
 
 
 
